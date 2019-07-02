03d67eb1f91a508a7c6c9991805caffe8c74f985
prostel <52460976+prostel@users.noreply.github.com>
2019-07-02 21:00:00 +0300

    Update functii.js

--- functii.js
+++ functii.js
@@ -1,11 +1,15 @@
 $(document).ready(
   function() {
-    
+      var randomNumber = Math.random();
+      if ( randomNumber == 1 )
+        $("body").prepend("<img src=\"prieten-rau.JPG\" alt=\"Prostel prieten rau\"> <p> Prostel este prieten rau. Purifica-l! </p>");
+      else
+        $("body").prepend("<img src=\"prieten-bun.JPG\" alt=\"Prostel prieten bun\"> <p> Prostel este prieten bun. </p>");
   }
 );
 
 function purifica() {
     $("img").remove();
     $("body").prepend("<img src=\"prieten-bun.JPG\" alt=\"Prostel prieten bun\">");
-    $("p").html("Prostel este prieten bun!");
+    $("p").html("Prostel este prieten bun.");
 }
