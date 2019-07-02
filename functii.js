3ad698fe2e0aed6f156490bce6811be83b988364
prostel <52460976+prostel@users.noreply.github.com>
2019-07-02 21:02:51 +0300

    Update functii.js

--- functii.js
+++ functii.js
@@ -1,7 +1,6 @@
 $(document).ready(
   function() {
-      var randomNumber = Math.random();
-      if ( randomNumber == 1 )
+      if ( Math.floor(Math.random() * 10 ) <=4 )
         $("body").prepend("<img src=\"prieten-rau.JPG\" alt=\"Prostel prieten rau\"> <p> Prostel este prieten rau. Purifica-l! </p>");
       else
         $("body").prepend("<img src=\"prieten-bun.JPG\" alt=\"Prostel prieten bun\"> <p> Prostel este prieten bun. </p>");
