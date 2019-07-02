3cfc927d19eb2dfea1251a69624f9481e1191c02
prostel <52460976+prostel@users.noreply.github.com>
2019-07-02 20:47:54 +0300

    Update functii.js

--- functii.js
+++ functii.js
@@ -6,4 +6,6 @@ $(document).ready(
 
 function purifica() {
     $("img").remove();
+    $("body").prepend("<img src="prieten-bun.JPG" alt="Prostel prieten bun">");
+    $("p").html("Prostel este prieten bun!");
 }
