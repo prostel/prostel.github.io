e763c2a91ed183a0a887f2a731f8d9194a4d6fc8
prostel <52460976+prostel@users.noreply.github.com>
2019-07-02 18:26:38 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title> PROSTEL </title>
+  </head>
+  <body>
+    <header> Binel ati venitel </header>
+  </body>
+</html>
