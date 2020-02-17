a1baa7fb56fa64a96fc07a5c9db4a5066bc848c6
prostel <52460976+prostel@users.noreply.github.com>
2020-02-17 13:57:17 +0200

    Add files via upload

---
+++ orange/jscript.js
@@ -0,0 +1,8 @@
+ $(document).ready(function() {
+  $('.panel-title').click(function() {
+  $(this).next().slideToggle();
+}); 
+$('.topic-title').click(function() {
+  $(this).next().slideToggle();
+}); 
+});
